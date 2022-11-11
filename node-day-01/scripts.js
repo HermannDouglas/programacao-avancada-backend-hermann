810db5db203954921a21b0d1cec5361004774cf6
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-11-10 20:39:18 -0500

    ok

--- node-day-01/scripts.js
+++ node-day-01/scripts.js
@@ -39,15 +39,98 @@
 // console.log('Comando que está após setTimeout.');
 
 
-function functionDate() {
-    const dt = new Date();
-    const h = dt.getHours();
-    const m = dt.getMinutes();
-    const s = dt.getSeconds();
-    console.log(`Hora: ${h}:${m}:${s}`);
-}
 
-setTimeout(functionDate, 5000);
-setTimeout(functionDate, 5000);
+// function functionDate() {
+//     const dt = new Date();
+//     const h = dt.getHours();
+//     const m = dt.getMinutes();
+//     const s = dt.getSeconds();
+//     console.log(`Hora: ${h}:${m}:${s}`);
+// }
+
 // setTimeout(functionDate, 5000);
-setInterval(functionDate, 5000);
+// setTimeout(functionDate, 5000);
+// // setTimeout(functionDate, 5000);
+// setInterval(functionDate, 5000);
+
+// Utilizando programação síncrona
+// function sum(callback, callbackErro) {
+//     const result = 1 + 1;
+//     if (result === 2) {
+//         callback();
+//     } else {
+//         callbackErro();
+//     }
+// }
+
+// function sucess() {
+//     console.log('Sucesso! A soma é 2.');
+// }
+
+// function erro() {
+//     console.log('A soma NÃO é 2. Alguma coisa deu errado...');
+// }
+
+// sum();
+
+// sum(
+//     () => (console.log('Sucesso! A soma é 2.')),
+//     () => (console.log('A soma NÃO é 2. Alguma coisa deu errado...'))
+// );
+// console.log('Testando exibição...');
+
+// Promise - é uma classe. promessa
+
+// const promise = new Promise((resolve, reject) => {
+//     const result = 1 + 2;
+//     if (result === 2) {
+//         resolve('Sucesso! A soma é 2.');
+//     } else {
+//         reject('A soma NÃO é 2. Alguma coisa deu errado...');
+//     }
+// });
+
+// promise.then(
+//     (resultado)=>(console.log(resultado)),
+//     (error)=>(console.log(error))
+// );
+// promise.catch((resultado)=>(console.log(resultado)));
+
+// voltando para callback
+
+// const name = 'Paulo Sampaio';
+// function whoIsTheBest(callback, callbackError) {
+//     if (name != 'Paulo Sampaio') {
+//         callbackError('Tá errado. Não tem como!');
+//     } else {
+//         callback({
+//             name: name,
+//             message: `Humildemente o melhor.`
+//         });
+//     }
+// }
+
+// whoIsTheBest(
+//     ((sucess) => (console.log(`${sucess.name} é ${sucess.message}`))),
+//     ((error) => (console.log(error)))
+// )
+
+
+// 
+const name = 'Paulo Sampaio';
+
+const promise = new Promise((resolve, reject) => {
+    if (name != 'Paulo Sampaio') {
+        reject('Tá errado. Não tem como!');
+    } else {
+        resolve({
+            name: name,
+            message: `Humildemente o melhor.`
+        });
+    }
+});
+
+promise.then(
+    ((sucess) => (console.log(`${sucess.name} é ${sucess.message}`))),
+    ((error) => (console.log(error)))
+);
