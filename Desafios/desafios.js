b5c39b669511523e5012a87ee42ea7cf244aff4f
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-12-10 10:21:53 -0500

    Organizando pastas dos desafios

--- Desafios/desafios.js
+++ Desafios/desafios.js
@@ -1,32 +1,32 @@
-// //  Desafio 01 ==================================================================
+//  Desafio  ==================================================================
 
-// // const num1 = new FormData(document.querySelector('form'));
-// // const inputValue1 = num1.get("num1");
-// // console.log(inputValue1);
+const num1 = new FormData(document.querySelector('form'));
+const inputValue1 = num1.get("num1");
+console.log(inputValue1);
 
-// // const num2 = new FormData(document.querySelector('form'));
-// // const inputValue2 = num2.get("num2");
-// // console.log(inputValue2);
+const num2 = new FormData(document.querySelector('form'));
+const inputValue2 = num2.get("num2");
+console.log(inputValue2);
 
-// // const valueConv1 = Number(inputValue1);
-// // const valueConv2 = Number(inputValue2);
-// // const soma = valueConv1 + valueConv2;
-// // // console.log(soma);
-// // // console.log(typeof soma);
+const valueConv1 = Number(inputValue1);
+const valueConv2 = Number(inputValue2);
+const soma = valueConv1 + valueConv2;
+// console.log(soma);
+// console.log(typeof soma);
 
-// // const subtracao = inputValue1 - inputValue2;
-// // // console.log(subtracao);
-// // // console.log(typeof subtracao);
+const subtracao = inputValue1 - inputValue2;
+// console.log(subtracao);
+// console.log(typeof subtracao);
 
-// // const multiplicacao = inputValue1 * inputValue2;
-// // // console.log(multiplicacao);
-// // // console.log(typeof multiplicacao);
+const multiplicacao = inputValue1 * inputValue2;
+// console.log(multiplicacao);
+// console.log(typeof multiplicacao);
 
-// // const divisao = inputValue1 / inputValue2;
-// // // console.log(divisao);
-// // // console.log(typeof divisao);
+const divisao = inputValue1 / inputValue2;
+// console.log(divisao);
+// console.log(typeof divisao);
 
-// Desafio 1: Armazene 5 dados numéricos em um vetor. Em seguida, copie para um objeto com as propriedades n1, n2, n3, n4 e n5.
+// Desafio: Armazene 5 dados numéricos em um vetor. Em seguida, copie para um objeto com as propriedades n1, n2, n3, n4 e n5.
 // Utilize destructuring para copiar os valores para 5 variáveis. Construa um objeto JSON com os valores das variáveis.
 
 const vectorNumerico = [1, 2, 3, 4, 5];
@@ -42,7 +42,7 @@ const { n1, n2, n3, n4, n5 } = vectorNumericoJson;
 console.log(JSON.stringify({ n1, n2, n3, n4, n5 }));
  
 
-// Desafio 2: Desenvolva uma calculadora que armazene em um objeto JSON o resultado das 4 operações básicas. 
+// Desafio: Desenvolva uma calculadora que armazene em um objeto JSON o resultado das 4 operações básicas. 
 // Em outras palavras: crie um objeto calculadora, inicialize cada uma das operações da calculadora (que serão 
 // propriedades do objeto) com o resultado do processamento matemático vindo de duas variáveis e converta para um objeto JSON.
 
