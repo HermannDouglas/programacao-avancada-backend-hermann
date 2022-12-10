37beb45f1b564f17915a3cdb1c57aa7c1a65baea
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-12-10 10:07:33 -0500

    Update desafios.js

--- Desafios/desafios.js
+++ Desafios/desafios.js
@@ -29,32 +29,32 @@
 // Desafio 1: Armazene 5 dados numéricos em um vetor. Em seguida, copie para um objeto com as propriedades n1, n2, n3, n4 e n5.
 // Utilize destructuring para copiar os valores para 5 variáveis. Construa um objeto JSON com os valores das variáveis.
 
-// const vectorNumerico = [1, 2, 3, 4, 5];
-// const vectorNumericoObject = {
-//     n1: vectorNumerico[0],
-//     n2: vectorNumerico[1],
-//     n3: vectorNumerico[2],
-//     n4: vectorNumerico[3],
-//     n5: vectorNumerico[4]
-// }
+const vectorNumerico = [1, 2, 3, 4, 5];
+const vectorNumericoObject = {
+    n1: vectorNumerico[0],
+    n2: vectorNumerico[1],
+    n3: vectorNumerico[2],
+    n4: vectorNumerico[3],
+    n5: vectorNumerico[4]
+}
 
-// const { n1, n2, n3, n4, n5 } = vectorNumericoJson;
-// console.log(JSON.stringify({ n1, n2, n3, n4, n5 }));
+const { n1, n2, n3, n4, n5 } = vectorNumericoJson;
+console.log(JSON.stringify({ n1, n2, n3, n4, n5 }));
  
 
 // Desafio 2: Desenvolva uma calculadora que armazene em um objeto JSON o resultado das 4 operações básicas. 
 // Em outras palavras: crie um objeto calculadora, inicialize cada uma das operações da calculadora (que serão 
 // propriedades do objeto) com o resultado do processamento matemático vindo de duas variáveis e converta para um objeto JSON.
 
-// const num1 = 10;
-// const num2 = 20;
-// const calculadora = {
-//     soma: num1 + num2,
-//     subtracao: num1 - num2,
-//     multiplicacao: num1 * num2,
-//     divisao: num1 / num2
-// }
-// console.log(JSON.stringify(calculadora));
+const num1 = 10;
+const num2 = 20;
+const calculadora = {
+    soma: num1 + num2,
+    subtracao: num1 - num2,
+    multiplicacao: num1 * num2,
+    divisao: num1 / num2
+}
+console.log(JSON.stringify(calculadora));
 
 // Desafio: Transforme a função a seguir em uma arrow function. ======================
 const resultado = (x) => () => (x+5);
@@ -62,15 +62,15 @@ console.log(resultado(7)());
 
 // Desafio da Hora
 
-// function functionDate() {
-//     const dt = new Date();
-//     const h = dt.getHours();
-//     const m = dt.getMinutes();
-//     const s = dt.getSeconds();
-//     console.log(`Hora: ${h}:${m}:${s}`);
-// }
+function functionDate() {
+    const dt = new Date();
+    const h = dt.getHours();
+    const m = dt.getMinutes();
+    const s = dt.getSeconds();
+    console.log(`Hora: ${h}:${m}:${s}`);
+}
 
+setTimeout(functionDate, 5000);
+setTimeout(functionDate, 5000);
 // setTimeout(functionDate, 5000);
-// setTimeout(functionDate, 5000);
-// // setTimeout(functionDate, 5000);
-// setInterval(functionDate, 5000);
+setInterval(functionDate, 5000);
